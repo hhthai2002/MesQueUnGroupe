0f5076d6674fcce20cf397bb4cce6102b950072e
Huynh Hieu Thai <104633789+hhthai2002@users.noreply.github.com>
2023-03-14 10:40:34 +0700

    Delete projectOJT directory

--- projectOJT/src/main/java/com/stackbug/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.stackbug.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-}
--- projectOJT/src/main/java/com/stackbug/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stackbug.repository;
-
-import com.stackbug.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-}
